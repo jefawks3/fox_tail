bf785e1ce1dec9b0e506576726460031d92033f7
James Fawks <jefawks3@gmail.com>
2023-09-22 15:31:41 -0400

    Adding the timeline component

---
+++ test/components/previews/timeline_component_preview.rb
@@ -0,0 +1,125 @@
+# frozen_string_literal: true
+
+# @logical_path components
+# @component Flowbite::TimelineComponent
+# @component Flowbite::Timeline::EntryComponent
+class TimelineComponentPreview < ViewComponent::Preview
+
+  # @!group Visuals
+
+  def dot_indicator
+    render Flowbite::TimelineComponent.new do |t|
+      t.with_entry do |e|
+        e.capture do
+          e.concat e.time_tag(Date.today - 10.days, class: "mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500")
+          e.concat e.content_tag(:h3, Faker::Books::Lovecraft.tome, class: "text-lg font-semibold text-gray-900 dark:text-white")
+          e.concat e.content_tag(:p, Faker::Books::Lovecraft.paragraph, class: "mb-4 text-base font-normal text-gray-500 dark:text-gray-400")
+        end
+      end
+      t.with_entry do |e|
+        e.with_dot_indicator color: :blue, animated: true
+        e.capture do
+          e.concat e.time_tag(Date.today - 20.days, class: "mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500")
+          e.concat e.content_tag(:h3, Faker::Books::Lovecraft.tome, class: "text-lg font-semibold text-gray-900 dark:text-white")
+          e.concat e.content_tag(:p, Faker::Books::Lovecraft.paragraph, class: "mb-4 text-base font-normal text-gray-500 dark:text-gray-400")
+        end
+      end
+      t.with_entry do |e|
+        e.capture do
+          e.concat e.time_tag(Date.today - 100.days, class: "mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500")
+          e.concat e.content_tag(:h3, Faker::Books::Lovecraft.tome, class: "text-lg font-semibold text-gray-900 dark:text-white")
+          e.concat e.content_tag(:p, Faker::Books::Lovecraft.paragraph, class: "mb-4 text-base font-normal text-gray-500 dark:text-gray-400")
+        end
+      end
+    end
+  end
+
+  def icons
+    render Flowbite::TimelineComponent.new do |t|
+      t.with_entry do |e|
+        e.with_icon
+        e.capture do
+          e.concat e.time_tag(Date.today - 10.days, class: "mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500")
+          e.concat e.content_tag(:h3, Faker::Books::Lovecraft.tome, class: "text-lg font-semibold text-gray-900 dark:text-white")
+          e.concat e.content_tag(:p, Faker::Books::Lovecraft.paragraph, class: "mb-4 text-base font-normal text-gray-500 dark:text-gray-400")
+        end
+      end
+      t.with_entry do |e|
+        e.with_icon "cake"
+        e.capture do
+          e.concat e.time_tag(Date.today - 20.days, class: "mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500")
+          e.concat e.content_tag(:h3, Faker::Books::Lovecraft.tome, class: "text-lg font-semibold text-gray-900 dark:text-white")
+          e.concat e.content_tag(:p, Faker::Books::Lovecraft.paragraph, class: "mb-4 text-base font-normal text-gray-500 dark:text-gray-400")
+        end
+      end
+      t.with_entry do |e|
+        e.with_icon
+        e.capture do
+          e.concat e.time_tag(Date.today - 100.days, class: "mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500")
+          e.concat e.content_tag(:h3, Faker::Books::Lovecraft.tome, class: "text-lg font-semibold text-gray-900 dark:text-white")
+          e.concat e.content_tag(:p, Faker::Books::Lovecraft.paragraph, class: "mb-4 text-base font-normal text-gray-500 dark:text-gray-400")
+        end
+      end
+    end
+  end
+
+  def images
+    render Flowbite::TimelineComponent.new do |t|
+      t.with_entry do |e|
+        e.with_image "users/bonnie-green.png"
+        e.capture do
+          e.concat e.time_tag(Date.today - 10.days, class: "mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500")
+          e.concat e.content_tag(:h3, Faker::Books::Lovecraft.tome, class: "text-lg font-semibold text-gray-900 dark:text-white")
+          e.concat e.content_tag(:p, Faker::Books::Lovecraft.paragraph, class: "mb-4 text-base font-normal text-gray-500 dark:text-gray-400")
+        end
+      end
+      t.with_entry do |e|
+        e.with_image "users/neil-sims.png"
+        e.capture do
+          e.concat e.time_tag(Date.today - 20.days, class: "mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500")
+          e.concat e.content_tag(:h3, Faker::Books::Lovecraft.tome, class: "text-lg font-semibold text-gray-900 dark:text-white")
+          e.concat e.content_tag(:p, Faker::Books::Lovecraft.paragraph, class: "mb-4 text-base font-normal text-gray-500 dark:text-gray-400")
+        end
+      end
+      t.with_entry do |e|
+        e.with_image "users/lana-byrd.png"
+        e.capture do
+          e.concat e.time_tag(Date.today - 100.days, class: "mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500")
+          e.concat e.content_tag(:h3, Faker::Books::Lovecraft.tome, class: "text-lg font-semibold text-gray-900 dark:text-white")
+          e.concat e.content_tag(:p, Faker::Books::Lovecraft.paragraph, class: "mb-4 text-base font-normal text-gray-500 dark:text-gray-400")
+        end
+      end
+    end
+  end
+
+  # @!endgroup
+
+  def stepper
+    render Flowbite::TimelineComponent.new vertical: false do |t|
+      t.with_entry do |e|
+        e.with_icon
+        e.capture do
+          e.concat e.time_tag(Date.today - 10.days, class: "mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500")
+          e.concat e.content_tag(:h3, Faker::Books::Lovecraft.tome, class: "text-lg font-semibold text-gray-900 dark:text-white")
+          e.concat e.content_tag(:p, Faker::Books::Lovecraft.paragraph, class: "mb-4 text-base font-normal text-gray-500 dark:text-gray-400")
+        end
+      end
+      t.with_entry do |e|
+        e.with_icon "cake"
+        e.capture do
+          e.concat e.time_tag(Date.today - 20.days, class: "mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500")
+          e.concat e.content_tag(:h3, Faker::Books::Lovecraft.tome, class: "text-lg font-semibold text-gray-900 dark:text-white")
+          e.concat e.content_tag(:p, Faker::Books::Lovecraft.paragraph, class: "mb-4 text-base font-normal text-gray-500 dark:text-gray-400")
+        end
+      end
+      t.with_entry do |e|
+        e.with_icon
+        e.capture do
+          e.concat e.time_tag(Date.today - 100.days, class: "mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500")
+          e.concat e.content_tag(:h3, Faker::Books::Lovecraft.tome, class: "text-lg font-semibold text-gray-900 dark:text-white")
+          e.concat e.content_tag(:p, Faker::Books::Lovecraft.paragraph, class: "mb-4 text-base font-normal text-gray-500 dark:text-gray-400")
+        end
+      end
+    end
+  end
+end
