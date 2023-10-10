665889315cb80092cf4e550132b53bc1f81c5797
James Fawks <jefawks3@gmail.com>
2023-10-10 15:54:23 -0400

    Moving the override files selection to the base component

--- app/components/fox_tail/base_component.rb
+++ app/components/fox_tail/base_component.rb
@@ -56,5 +56,42 @@ class FoxTail::BaseComponent < FoxTail::Base
     def stimulus_merger
       FoxTail::Base.fox_tail_config.stimulus_merger
     end
+
+    def override_files(extensions)
+      # Taken from ViewComponent::Base.sidecar_files
+      directory = override_directory
+      filename = File.basename(source_location, ".rb")
+      component_name = name.demodulize.underscore
+      extensions = Array(extensions).join(",")
+
+      # Add support for nested components defined in the same file.
+      #
+      # for example
+      #
+      # class MyComponent < ViewComponent::Base
+      #   class MyOtherComponent < ViewComponent::Base
+      #   end
+      # end
+      #
+      # Without this, `MyOtherComponent` will not look for `my_component/my_other_component.html.erb`
+      nested_component_files =
+        if name.include?("::") && component_name != filename
+          Dir["#{directory}/#{filename}/#{component_name}.*{#{extensions}}"]
+        else
+          []
+        end
+
+      # view files in the same directory as the component
+      sidecar_files = Dir["#{directory}/#{component_name}.*{#{extensions}}"]
+
+      sidecar_directory_files = Dir["#{directory}/#{component_name}/#{filename}.*{#{extensions}}"]
+
+      (sidecar_files - [source_location] + sidecar_directory_files + nested_component_files).uniq
+    end
+
+    def override_directory
+      dir = FoxTail::Base.fox_tail_config.theme_path || ViewComponent::Base.config.view_component_path
+      Rails.root.join dir, File.dirname(virtual_path).gsub(/^\/+/, "")
+    end
   end
 end
--- app/components/fox_tail/concerns/has_theme.rb
+++ app/components/fox_tail/concerns/has_theme.rb
@@ -28,39 +28,7 @@ module FoxTail::Concerns::HasTheme
     end
 
     def custom_theme_files
-      # Taken from ViewComponent::Base.sidecar_files
-      directory = override_directory
-      filename = File.basename(source_location, ".rb")
-      component_name = name.demodulize.underscore
-
-      # Add support for nested components defined in the same file.
-      #
-      # for example
-      #
-      # class MyComponent < ViewComponent::Base
-      #   class MyOtherComponent < ViewComponent::Base
-      #   end
-      # end
-      #
-      # Without this, `MyOtherComponent` will not look for `my_component/my_other_component.html.erb`
-      nested_component_files =
-        if name.include?("::") && component_name != filename
-          Dir["#{directory}/#{filename}/#{component_name}.*{#{THEME_EXTENSION}}"]
-        else
-          []
-        end
-
-      # view files in the same directory as the component
-      sidecar_files = Dir["#{directory}/#{component_name}.*{#{THEME_EXTENSION}}"]
-
-      sidecar_directory_files = Dir["#{directory}/#{component_name}/#{filename}.*{#{THEME_EXTENSION}}"]
-
-      (sidecar_files - [source_location] + sidecar_directory_files + nested_component_files).uniq
-    end
-
-    def override_directory
-      dir = FoxTail::Base.fox_tail_config.theme_path || ViewComponent::Base.config.view_component_path
-      Rails.root.join dir, File.dirname(virtual_path).gsub(/^\/+/, "")
+      override_files [THEME_EXTENSION]
     end
   end
 end
