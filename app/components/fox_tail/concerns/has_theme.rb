35015bfdf897c30f934eea298f1998bc9d1456ce
James Fawks <jefawks3@gmail.com>
2023-10-12 15:46:32 -0400

    Simplifying the theming

--- app/components/fox_tail/concerns/has_theme.rb
+++ app/components/fox_tail/concerns/has_theme.rb
@@ -3,32 +3,17 @@
 module FoxTail::Concerns::HasTheme
   extend ActiveSupport::Concern
 
-  THEME_EXTENSION = "theme.yml"
-
   def theme
     @theme ||= self.class.theme
   end
 
   class_methods do
-    def theme(merge_base: true)
+    def theme
       theme = FoxTail::Theme.new
-      theme.merge! superclass.theme if merge_base && superclass.respond_to?(:theme)
-      theme_files.each { |file| theme.merge! file }
+      theme.merge! superclass.theme if superclass.respond_to?(:theme)
+      theme_file = sidecar_files(["theme.yml"]).first
+      theme.merge! theme_file if theme_file.present?
       theme
-    end
-
-    private
-
-    def theme_files
-      (component_theme_files + custom_theme_files).uniq.select(&:present?)
-    end
-
-    def component_theme_files
-      sidecar_files [THEME_EXTENSION]
-    end
-
-    def custom_theme_files
-      override_files [THEME_EXTENSION]
     end
   end
 end
