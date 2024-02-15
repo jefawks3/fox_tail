fb8d553de5dec4cb23333916cf7c904fd7326ba5
James Fawks <jefawks3@gmail.com>
2024-02-15 10:27:45 -0500

    Adding the formable capabilities

--- app/components/fox_tail/checkbox_component.rb
+++ app/components/fox_tail/checkbox_component.rb
@@ -1,18 +1,53 @@
 # frozen_string_literal: true
 
 class FoxTail::CheckboxComponent < FoxTail::BaseComponent
+  include FoxTail::Concerns::Formable
+
   has_option :color, default: :default
   has_option :size, default: :base
+  has_option :value
   has_option :unchecked_value, default: "0"
   has_option :checked_value, default: "1"
   has_option :include_hidden, type: :boolean, default: true
+  has_option :multiple, type: :boolean, default: false
+  has_option :checked, type: :boolean
+
+  def value
+    options[:value] ||= value_from_object
+  end
+
+  def checked?
+    return !!options[:checked] if options.key? :checked
+
+    case value
+    when TrueClass, FalseClass
+      value == !!checked_value
+    when NilClass
+      false
+    when String
+      value == checked_value
+    else
+      if value.respond_to?(:include?)
+        value.map(&:to_s).include?(checked_value.to_s)
+      else
+        value.to_i == checked_value.to_i
+      end
+    end
+  end
 
   def before_render
     super
 
+    if multiple?
+      add_default_name_and_id_for_value checked_value
+    else
+      add_default_name_and_id
+    end
+
     html_attributes[:type] = :checkbox
     html_attributes[:class] = classnames theme.apply(:root, self), html_class
     html_attributes[:value] = checked_value
+    html_attributes[:checked] = checked?
   end
 
   def call
