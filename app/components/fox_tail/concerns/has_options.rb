b6bc5db0b2d2aa7d7e179c9c71adeb457f6612d6
James Fawks <jefawks3@gmail.com>
2024-02-15 10:34:02 -0500

    Adding the `include_options_from` to easily include options from a dependent component.

--- app/components/fox_tail/concerns/has_options.rb
+++ app/components/fox_tail/concerns/has_options.rb
@@ -22,29 +22,52 @@ module FoxTail::Concerns::HasOptions
 
   class_methods do
     def default_options
-      registered_options.reject { |_,v| v[:default].blank? }.transform_values { |v| v[:default] }
+      registered_options.reject { |_,v| v[:default].nil? }.transform_values { |v| v[:default] }
     end
 
     def has_option(name, as: name, default: nil, type: nil)
       raise FoxTail::ReservedOption.new(self, as) if RESERVED_OPTIONS.include?(as.to_s)
 
-      self.registered_options = registered_options.merge name => { name: name, as: as, default: default, type: type }
+      register_option name, { name: name, as: as, default: default, type: type }
+    end
 
-      define_method as do
-        options[name]
+    def include_options_from(klass, only: nil, except: nil)
+      return unless klass.respond_to? :registered_options
+
+      only = Array(only).map(&:to_sym) if only.present?
+      except = Array(except).map(&:to_sym) if except.present?
+
+      klass.registered_options.each do |k, v|
+        if !registered_options.key?(k) && (only.nil? || only.include?(k)) && (except.nil? || !except.include?(k))
+          register_option k, v
+        end
       end
 
-      define_method :"#{as}?" do
-        if type == :boolean
-          !!send(as)
-        else
-          send(as).present?
-        end
+      define_method "#{klass.name.underscore.gsub("_component", "").gsub("/", "_")}_options" do
+        options.slice(*klass.registered_options.keys)
       end
     end
 
     def extract_options(options = {})
       options.slice(*registered_options.keys)
+    end
+
+    private
+
+    def register_option(name, option)
+      self.registered_options = registered_options.merge name => option
+
+      define_method option[:as] do
+        options[name]
+      end
+
+      define_method :"#{option[:as]}?" do
+        if option[:type] == :boolean
+          !!send(option[:as])
+        else
+          send(option[:as]).present?
+        end
+      end
     end
   end
 end
