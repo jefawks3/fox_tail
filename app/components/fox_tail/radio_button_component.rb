dcb433a856b1b02adf62c10401b024fcc0034b69
James Fawks <jefawks3@gmail.com>
2024-02-15 10:30:47 -0500

    Adding the formable capabilities

--- app/components/fox_tail/radio_button_component.rb
+++ app/components/fox_tail/radio_button_component.rb
@@ -1,14 +1,31 @@
 # frozen_string_literal: true
 
 class FoxTail::RadioButtonComponent < FoxTail::BaseComponent
+  include FoxTail::Concerns::Formable
+
   has_option :color, default: :default
   has_option :size, default: :base
+  has_option :value
+  has_option :checked, as: :boolean, default: false
+
+  def value
+    options[:value] ||= value_from_object
+  end
+
+  def checked?
+    return !!options[:checked] if options.key? :checked
+    return false unless object.present?
+
+    value.to_s == value_from_object.to_s
+  end
 
   def before_render
     super
 
+    add_default_name_and_id_for_value value
     html_attributes[:type] = :radio
     html_attributes[:class] = classnames theme.apply(:root, self), html_class
+    html_attributes[:checked] = checked?
   end
 
   def call
