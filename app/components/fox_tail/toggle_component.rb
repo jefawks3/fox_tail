33ba9e24d195ded3fafd6d059a3132d41444ed2a
James Fawks <jefawks3@gmail.com>
2024-02-15 10:29:24 -0500

    Inheriting from the `CheckboxComponent`.

--- app/components/fox_tail/toggle_component.rb
+++ app/components/fox_tail/toggle_component.rb
@@ -1,22 +1,20 @@
 # frozen_string_literal: true
 
-class FoxTail::ToggleComponent < FoxTail::BaseComponent
-  has_option :size, default: :base
-  has_option :color, default: :default
-
+class FoxTail::ToggleComponent < FoxTail::CheckboxComponent
   renders_one :label, lambda { |text| content_tag :span, text, class: theme.apply(:label, self) }
 
   def before_render
     super
 
-    html_attributes[:type]
+    html_attributes[:class] = theme.apply(:root, self)
   end
 
   def call
     content_tag :label, container_attributes do
-      concat content_tag(:input, nil, input_attributes)
+      concat super
       concat content_tag(:div, nil, toggle_attributes)
       concat label if label?
+      concat content if content?
     end
   end
 
@@ -24,10 +22,6 @@ class FoxTail::ToggleComponent < FoxTail::BaseComponent
 
   def container_attributes
     { class: classnames(theme.apply(:container, self), html_class) }
-  end
-
-  def input_attributes
-    html_attributes.merge type: :checkbox, class: theme.apply(:root, self)
   end
 
   def toggle_attributes
