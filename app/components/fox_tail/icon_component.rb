1196a99010510739d218806fddebc770963b3dec
James Fawks <jefawks3@gmail.com>
2024-08-06 17:27:08 -0400

    Adding the `shape` option

--- app/components/fox_tail/icon_component.rb
+++ app/components/fox_tail/icon_component.rb
@@ -7,6 +7,19 @@ class FoxTail::IconComponent < FoxTail::IconBaseComponent
 
   has_option :color, default: :default
   has_option :size, default: :base
+  has_option :shape, default: :none
+
+  def shape?
+    shape != "none"
+  end
+
+  def call
+    if shape?
+      content_tag :div, super, class: theme.apply(:container, self)
+    else
+      super
+    end
+  end
 
   protected
 
